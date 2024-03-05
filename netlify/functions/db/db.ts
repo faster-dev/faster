b90ab41e45bbd02ae7bd4cef8cdf12e3f069d0bd
Nicholas Piano <nicholas.d.piano@gmail.com>
2024-03-05 18:45:28 +0100

    fix: cert string

--- netlify/functions/db/db.ts
+++ netlify/functions/db/db.ts
@@ -1,7 +1,8 @@
-import fs from 'fs';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import pg from 'pg';
 import * as schema from './schema';
+
+const certString = process.env.CA_CERTIFICATE || '';
 
 export const getDatabase = async () => {
   const client = new pg.Client({
@@ -11,7 +12,7 @@ export const getDatabase = async () => {
     database: process.env.DATABASE_NAME,
     port: parseInt(process.env.DATABASE_PORT || '0', 10),
     ssl: {
-      ca: fs.readFileSync('ca-certificate.crt'),
+      ca: Buffer.from(certString.replace(/\\n/g, '\n')),
     },
   });
   await client.connect();
